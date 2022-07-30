3959929b3f18a1a23eeb67ce83dbc008d32f13d4
闫韬蒙 <3094969938@qq.com>
2022-07-30 22:35:26 +0800

    docs: 添加了设置过期时间的注释

--- src/setMethods/initSetTime.ts
+++ src/setMethods/initSetTime.ts
@@ -1,13 +1,16 @@
 interface KeyValueObject {
   [key: string]: any;
 }
+// 初始化设置过去时间的方法
 export function initSetTime(vm: any): void {
   vm.setTime = function (
     params: KeyValueObject,
     expiration: number | string
   ): void {
     let { constructor } = this;
+// 加载类上的插件方法
     expiration = constructor.plugins.time(expiration);
+// 类型检验
     let typeList: string[] = ["number", "string"];
     if (
       typeList.every((item) => {
@@ -16,6 +19,7 @@ export function initSetTime(vm: any): void {
     ) {
       return;
     }
+// 遍历设置过期时间
     for (let item in params) {
       params.hasOwnProperty(item)
         ? localStorage.setItem(
